1e7d6d5e1f4b15b8053738fc8eb617563accd2f2
Jeffrey Andersen <jeffrewa@uci.edu>
2025-03-14 17:55:53 +0000

    Fixed Print Function To Work With Gradescope

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -43,11 +43,7 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 
 void print_path(const vector<int>& v, int total) {
     for (size_t i = 0; i < v.size(); i++) {
-        if (i != v.size() - 1) {
-            cout << v[i] << "->";
-        } else {
-            cout << v[i] << endl;;
-        }
+        cout << v[i] << " ";
     }
     cout << "Total Cost Is " << total << endl;
 }
