f2809e8ebe4b849f618d32c10a3306dcbfb00b06
Jeffrey Andersen <jeffrewa@uci.edu>
2025-03-14 20:01:12 +0000

    fixed print?

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -111,9 +111,11 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); i++) {
         cout << ladder[i] << " ";
     }
+    cout << endl;
 }
 
 void verify_word_ladder() {
